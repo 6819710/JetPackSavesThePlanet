1ad55dbea15af06d41b5fb2275eae9c1dbd519c9
6819710 <jpgalovic@gmail.com>
2017-08-08 00:40:01 +1000

    Add: breathing script
    breatheing happens on a set timer, time between breaths can be changed.

---
+++ Assets/Scripts/Breathing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breathing : MonoBehaviour {
+    // Refence Variables
+    private Oxygen oxygenSupply;
+
+    // Settings
+    public float breathingRate; // How many frames need to pass between each use of oxygen
+
+    // Misc
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+        oxygenSupply = this.gameObject.GetComponent<Oxygen>();
+        timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (oxygenSupply != null)
+        {
+            if (timer >= breathingRate)
+            {
+                oxygenSupply.ApplyDelta(-1);
+                timer = 0;
+            }
+            else
+                timer += Time.deltaTime;
+        }
+	}
+}
