85523794500b03867a96cb301261cdf935bf92ba
xxfast <isuru@xfastgames.com>
2017-08-09 09:51:56 +1000

    resolved conflicts

---
+++ Assets/Scripts/OxygenLevel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OxygenLevel : MonoBehaviour {
+
+    public Oxygen oxygen;
+    public Image context;
+
+    private float fillAmount;
+
+    private int maxOxygen;
+    private int currentOxygen;
+
+	// Use this for initialization
+	void Start () {
+        maxOxygen = oxygen.maxOxygen;
+        currentOxygen = oxygen.oxygen;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(oxygen.oxygen != currentOxygen) // only process if current oxygen has changed
+        {
+            currentOxygen = oxygen.oxygen;
+            fillAmount = 1.0f * ((float)currentOxygen / (float)maxOxygen);
+            context.fillAmount = fillAmount;
+        }
+	}
+}
