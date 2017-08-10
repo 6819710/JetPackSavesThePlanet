16cde2257b3432cf21049a604c9754eb26b2bc1c
xxfast <isuru@xfastgames.com>
2017-08-10 19:56:06 +1000

    updated so that jetpack only exhausts when moving

--- Assets/Scripts/SwipeToMove.cs
+++ Assets/Scripts/SwipeToMove.cs
@@ -25,21 +25,26 @@ public class SwipeToMove : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		Transform jetpack = transform.Find ("jetpack");
 		if(directionalIndicator!=null) directionalIndicator.GetComponent<SpriteRenderer> ().enabled = false;
-		if( Input.touchCount > 0 && Input.GetTouch(0).phase==TouchPhase.Moved){
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
+			jetpack.gameObject.GetComponent<ParticleSystem> ().Play ();
 			Vector2 touchDirection = Input.GetTouch (0).deltaPosition.normalized;
 			if (changeDirection) {
 				Vector2 scale = originalScale;
-				if (touchDirection.x < 0) scale.x *= -1;
+				if (touchDirection.x < 0)
+					scale.x *= -1;
 				this.transform.localScale = scale;
 			}
 			touchDirection.Scale (new Vector2 (speed, speed));
 			this.gameObject.GetComponent<Rigidbody2D> ().AddForce (touchDirection);
-			if (directionalIndicator!=null) {
+			if (directionalIndicator != null) {
 				directionalIndicator.GetComponent<SpriteRenderer> ().enabled = true;
-				float angle = Mathf.Atan2(this.gameObject.GetComponent<Rigidbody2D> ().velocity.x, -this.gameObject.GetComponent<Rigidbody2D> ().velocity.y) * Mathf.Rad2Deg;
-				directionalIndicator.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+				float angle = Mathf.Atan2 (this.gameObject.GetComponent<Rigidbody2D> ().velocity.x, -this.gameObject.GetComponent<Rigidbody2D> ().velocity.y) * Mathf.Rad2Deg;
+				directionalIndicator.transform.rotation = Quaternion.AngleAxis (angle - 90, Vector3.forward);
 			}
+		} else {
+			jetpack.gameObject.GetComponent<ParticleSystem> ().Stop ();
 		}
 	}
 }
