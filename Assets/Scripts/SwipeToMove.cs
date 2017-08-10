f3771ff9ee8b1cd2782416dbfa6fe2d8a767a190
xxfast <isuru@xfastgames.com>
2017-08-10 15:36:51 +1000

    added the swipe to move behaviour script

---
+++ Assets/Scripts/SwipeToMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeToMove : MonoBehaviour {
+	
+	public float speed = 100;
+	public bool changeDirection = false;
+	private Vector2 originalScale;
+
+	// Use this for initialization
+	void Start () {
+		this.gameObject.GetComponent<Rigidbody2D> ().freezeRotation = true;
+		originalScale = this.gameObject.transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( Input.touchCount > 0 && Input.GetTouch(0).phase==TouchPhase.Moved){
+			Vector2 touchDirection = Input.GetTouch (0).deltaPosition.normalized;
+			if (changeDirection) {
+				Vector2 scale = originalScale;
+			if (touchDirection.x < 0)
+				scale.x *= -1;
+				this.transform.localScale = scale;
+			}
+			touchDirection.Scale (new Vector2 (speed, speed));
+			this.gameObject.GetComponent<Rigidbody2D> ().AddForce (touchDirection);
+		}
+	}
+}
