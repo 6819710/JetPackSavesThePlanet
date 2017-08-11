68952324ebfd55f96cbda4de4e1722ff675f9a39
xxfast <isuru@xfastgames.com>
2017-08-11 11:00:07 +1000

    updated with thresholds and stuff
    added the thruster code to thruster.cs

--- Assets/Scripts/SwipeToMove.cs
+++ Assets/Scripts/SwipeToMove.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class SwipeToMove : MonoBehaviour {
 	
 	public float speed = 100;
-	public bool changeDirection = false;
+	public bool flipToDirection = false;
 	public GameObject directionalIndicator;
+	public float indicatorThreshold = 1.0f;
 
 	private Vector2 originalScale;
 
 	public bool isMoving
 	{
 		get { 
-			return (this.gameObject.GetComponent<Rigidbody2D> ().velocity != Vector2.zero); 
+			return (this.gameObject.GetComponent<Rigidbody2D> ().velocity.magnitude > indicatorThreshold); 
 		}
 	}
 
@@ -25,26 +26,36 @@ public class SwipeToMove : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		Transform jetpack = transform.Find ("jetpack");
-		if(directionalIndicator!=null) directionalIndicator.GetComponent<SpriteRenderer> ().enabled = false;
 		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Moved) {
-			jetpack.gameObject.GetComponent<ParticleSystem> ().Play ();
+			// Get a normalised touch input vector
 			Vector2 touchDirection = Input.GetTouch (0).deltaPosition.normalized;
-			if (changeDirection) {
+			// If flipToDirection is enable, flip the localscale of the player to match direction 
+			if (flipToDirection) {
 				Vector2 scale = originalScale;
 				if (touchDirection.x < 0)
 					scale.x *= -1;
 				this.transform.localScale = scale;
 			}
+			// multiply the touch vector with a speed vector 
 			touchDirection.Scale (new Vector2 (speed, speed));
+			// Force based movement
 			this.gameObject.GetComponent<Rigidbody2D> ().AddForce (touchDirection);
+			// If the indicator is available
 			if (directionalIndicator != null) {
-				directionalIndicator.GetComponent<SpriteRenderer> ().enabled = true;
-				float angle = Mathf.Atan2 (this.gameObject.GetComponent<Rigidbody2D> ().velocity.x, -this.gameObject.GetComponent<Rigidbody2D> ().velocity.y) * Mathf.Rad2Deg;
-				directionalIndicator.transform.rotation = Quaternion.AngleAxis (angle - 90, Vector3.forward);
+				
 			}
+		} 
+
+		// If the indicator is available enable it's rendering
+		// some math magic to get the indicator to point at the direction where the player is moving 
+		if (this.gameObject.GetComponent<Rigidbody2D> ().velocity.magnitude > indicatorThreshold) {
+			directionalIndicator.GetComponent<SpriteRenderer> ().enabled = true;
+			float angle = Mathf.Atan2 (this.gameObject.GetComponent<Rigidbody2D> ().velocity.x, -this.gameObject.GetComponent<Rigidbody2D> ().velocity.y) * Mathf.Rad2Deg;
+			directionalIndicator.transform.rotation = Quaternion.AngleAxis (angle - 90, Vector3.forward);
 		} else {
-			jetpack.gameObject.GetComponent<ParticleSystem> ().Stop ();
+			// disable it's rendering
+			if (directionalIndicator != null)
+				directionalIndicator.GetComponent<SpriteRenderer> ().enabled = false;
 		}
 	}
 }
--- Assets/Scripts/Thruster.cs
+++ Assets/Scripts/Thruster.cs
@@ -13,12 +13,14 @@ public class Thruster : MonoBehaviour {
 
     // Misc
     private float timer;
+	private Transform jetpack;
 
     // Use this for initialization
     void Start()
     {
         oxygenSupply = this.gameObject.GetComponent<Oxygen>();
 		playerMovement = this.gameObject.GetComponent<SwipeToMove>();
+		jetpack = transform.Find ("jetpack");
         timer = 0;
     }
 
@@ -29,16 +31,16 @@ public class Thruster : MonoBehaviour {
         {
             if(playerMovement != null)
             {
-                if(playerMovement.isMoving)
-                {
-                    if (timer >= depletionRate)
-                    {
-                        oxygenSupply.ApplyDelta(-depletionAmount);
-                        timer = 0;
-                    }
-                    else
-                        timer += Time.deltaTime;
-                }
+				if (playerMovement.isMoving) {
+					jetpack.gameObject.GetComponent<ParticleSystem> ().Play ();
+					if (timer >= depletionRate) {
+						oxygenSupply.ApplyDelta (-depletionAmount);
+						timer = 0;
+					} else
+						timer += Time.deltaTime;
+				} else {
+					jetpack.gameObject.GetComponent<ParticleSystem> ().Stop ();
+				}
             }
         }
     }
