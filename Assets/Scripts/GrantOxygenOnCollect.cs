13222d5ae4dced4f0d03a8f8f623d0a088d0ea92
xxfast <isuru@xfastgames.com>
2017-08-13 20:32:38 +1000

    added the file grant oxygen on collect

---
+++ Assets/Scripts/GrantOxygenOnCollect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class GrantOxygenOnCollect : MonoBehaviour {
+
+	public List<string> canBeCollectedByEntitesTagged;
+	public Oxygen self;
+
+	void Awake(){
+		if (canBeCollectedByEntitesTagged!=null)
+			canBeCollectedByEntitesTagged = new List<string> ();
+		self = gameObject.GetComponent<Oxygen>();
+	}
+
+	void OnCollisionEnter2D(Collision2D collision) {
+		if(canBeCollectedByEntitesTagged.Contains(collision.gameObject.tag)){
+			// Trigger Oxygen Replenishment
+			Oxygen oxygen = collision.gameObject.GetComponent<Oxygen>();
+			if (oxygen != null && self != null) oxygen.ApplyDelta(self.oxygen);
+		}
+	}
+}
