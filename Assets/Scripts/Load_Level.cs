013df0f4cfd408ce1451abd63746e103744a7eac
Xavier-Gibbon <101104563@student.swin.edu.au>
2017-08-14 14:09:21 +1000

    Added documentation

--- Assets/Scripts/Load_Level.cs
+++ Assets/Scripts/Load_Level.cs
@@ -7,9 +7,12 @@ public class Load_Level : MonoBehaviour {
 
     public string SceneName;
 
+    //When colliding, it will load the given level
+    //NOTE: When loading a level, the scene must be added to the build settings
+    //      this can be done by going into file/Build Settings
     void OnCollisionEnter2D(Collision2D coll)
     {
-        if (SceneName != null)
+        if (SceneName != null && coll.gameObject.CompareTag("player"))
         {
             SceneManager.LoadScene(SceneName);
         }  
