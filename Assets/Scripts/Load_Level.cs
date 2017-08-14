9d2fea08265265cd6a77997047066b033a45d220
Xavier-Gibbon <101104563@student.swin.edu.au>
2017-08-14 14:05:01 +1000

    Added the start meteor

---
+++ Assets/Scripts/Load_Level.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Load_Level : MonoBehaviour {
+
+    public string SceneName;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (SceneName != null)
+        {
+            SceneManager.LoadScene(SceneName);
+        }  
+    }
+}
