979b9bd424747299ca8e03e614b35cde10e8958a
xxfast <isuru@xfastgames.com>
2017-08-27 21:03:23 +1000

    added the game manager, and the state manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	private StateManager stateManager; 
+
+	public StateManager StateManager {
+		get {
+			return stateManager;
+		}
+		set {
+			stateManager = value;
+		}
+	}
+
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy(gameObject); 
+
+		DontDestroyOnLoad(gameObject);
+		stateManager = this.GetComponent<StateManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/StateManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour {
+
+	public GameState state;
+
+	public GameState State {
+		get {
+			return state;
+		}
+		set {
+			state = value;
+		}
+	}
+
+	void Start () {
+		state = GameState.Loading;
+	}
+
+	public void Change(GameState toChange){
+		State = toChange;
+	}
+
+}
