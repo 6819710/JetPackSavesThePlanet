5a14f4c185bc7818fde67d884a974172809265f5
James P. Galovic <jpgalovic@gmail.com>
2017-09-04 23:36:04 +1000

    Add: Oxygen Triggers, Low Oxygen
    Low oxygen is set to 50%, when level drops below 50% music changes to panic, when xugen is higher than 50%

---
+++ Assets/Scripts/OxygenTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenTrigger : MonoBehaviour {
+
+    private Oxygen oxygen;
+
+    public float low;
+
+    private int triggerLow;
+
+	// Use this for initialization
+	void Start () {
+		oxygen = gameObject.GetComponent<Oxygen>();
+        triggerLow = (int)(oxygen.maxOxygen * low); //Sets low oxygen threshold
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if(oxygen.oxygen < triggerLow)
+        {
+            GameObject.Find("Music").SendMessage("toStress");
+        }
+        else if (oxygen.oxygen > triggerLow)
+        {
+            GameObject.Find("Music").SendMessage("toCalm");
+        }
+    }
+}
