f716fbd7f79e080d45537cd3ca2fe9a8c5f3f65f
James P. Galovic <jpgalovic@gmail.com>
2017-09-04 23:59:23 +1000

    Change: Added Critical Oxygen Trigger Level
    Trigger Level acts in same way as low level, low level changed to be in between levels

--- Assets/Scripts/OxygenTrigger.cs
+++ Assets/Scripts/OxygenTrigger.cs
@@ -7,18 +7,25 @@ public class OxygenTrigger : MonoBehaviour {
     private Oxygen oxygen;
 
     public float low;
+    public float critical;
 
     private int triggerLow;
+    private int triggerCritical;
 
 	// Use this for initialization
 	void Start () {
 		oxygen = gameObject.GetComponent<Oxygen>();
         triggerLow = (int)(oxygen.maxOxygen * low); //Sets low oxygen threshold
+        triggerCritical = (int)(oxygen.maxOxygen * critical);
     }
 	
 	// Update is called once per frame
 	void Update () {
-		if(oxygen.oxygen < triggerLow)
+        if (oxygen.oxygen < triggerCritical)
+        {
+            GameObject.Find("Music").SendMessage("toStressBeat");
+        }
+        else if(oxygen.oxygen < triggerLow && oxygen.oxygen >= critical)
         {
             GameObject.Find("Music").SendMessage("toStress");
         }
