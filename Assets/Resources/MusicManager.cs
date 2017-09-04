ca8bf6c04f906e168feb9aef430c007bce49bc2b
xxfast <isuru@xfastgames.com>
2017-09-04 17:43:57 +1000

    Added the music manager to manage music from game states

---
+++ Assets/Resources/MusicManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(StateManager))]
+public class MusicManager : MonoBehaviour {
+
+	private MusicController musicController;
+	private StateManager stateManager;
+
+	public GameState shown;
+
+	// Use this for initialization
+	void Start () {
+		GameObject music = this.transform.Find("Music").gameObject;
+		if(music!=null){
+			musicController = music.GetComponent<MusicController>();
+		}
+		stateManager = this.gameObject.GetComponent<StateManager> ();
+		shown = stateManager.CurrentState;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(shown!=stateManager.CurrentState){
+			shown = stateManager.CurrentState;
+			switch(shown){
+				case GameState.Menu:
+					musicController.toMenu ();
+					break;
+				case GameState.Playing:
+					musicController.toMenu ();
+					break;
+				case GameState.Paused:
+					musicController.toCalmBeat ();
+					break;
+			}
+		}
+		
+	}
+}
