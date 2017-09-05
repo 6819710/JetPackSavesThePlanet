1c7cdb7a128b52b6b80895488d579866d71daeee
xxfast <isuru@xfastgames.com>
2017-09-05 18:21:34 +1000

    Added the upgrade system

---
+++ Assets/Scripts/UpgradeSystem.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeSystem : MonoBehaviour {
+
+	public List<Upgrade> upgrades;
+
+	public List<Upgrade> Upgrades {
+		get {
+			return upgrades;
+		}
+		set {
+			upgrades = value;
+		}
+	}
+
+	public Upgrade this[int index]    
+	{  
+		get { return upgrades[index]; }
+	}  
+
+	void Start(){
+		foreach(Upgrade u in upgrades){
+			u.Owner = this.gameObject;
+		}
+	}
+
+	void Update () {
+		
+	}
+
+	public void Activate(Upgrade upgrade){
+		upgrade.Activate ();
+	}
+
+	public void Add(Upgrade upgrade){
+		upgrade.Owner = this.gameObject;
+		upgrades.Add (upgrade);
+	}
+
+	public void Remove(Upgrade upgrade){
+		upgrades.Remove (upgrade);
+	}
+
+	public void RemoveAt(int index){
+		upgrades.RemoveAt (index);
+	}
+
+	public void Clear(){
+		upgrades.Clear ();
+	}
+}
