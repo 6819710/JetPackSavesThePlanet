77217d679a6fef889de4868dd193262fb7b4125d
xxfast <isuru@xfastgames.com>
2017-09-07 02:02:47 +1000

    updated the shield to stunt to snake
    need work for the shield itself

---
+++ Assets/StuntOnCollision.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StuntOnCollision : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D other) {
+		StuntingBehavior sb = other.GetComponent<StuntingBehavior> ();
+		if (sb != null)
+			sb.Stunt ();
+	}
+}
