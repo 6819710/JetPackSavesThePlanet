de6f88a66f671bc2b75ab6e7556af8c7e0766a04
xxfast <isuru@xfastgames.com>
2017-09-11 17:51:19 +0800

    added the player manager to facilitate multiple player in the future if needed be

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+	public GameObject player;
+
+	public GameObject this [int i]
+	{
+		get { return player; }
+	}
+
+	void Start () {
+		Initialise ();
+	}
+
+	public void Initialise ()
+	{
+		if (player == null)
+			player = GameObject.Find ("Player");
+	}
+
+}
