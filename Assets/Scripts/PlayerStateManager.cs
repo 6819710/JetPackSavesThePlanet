bf276fb4f35cefabafc6acdd9b224fcd51c034a9
xxfast <isuru@xfastgames.com>
2017-09-11 15:59:53 +0800

    merged master

---
+++ Assets/Scripts/PlayerStateManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateManager : MonoBehaviour {
+
+	public PlayerState state = PlayerState.Idle;
+
+	public PlayerState State {
+		get {
+			return state;
+		}
+		set {
+			state = value;
+		}
+	}
+	
+	public void Idle(){
+		state = PlayerState.Idle;
+	}
+
+	public void Happy(){
+		state = PlayerState.Happy;
+	}
+
+	public void Panic(){
+		state = PlayerState.Panic;
+	}
+
+	public void Suffocating(){
+		state = PlayerState.Suffocating;
+	}
+
+	public void Dead(){
+		state = PlayerState.Dead;
+	}
+}
