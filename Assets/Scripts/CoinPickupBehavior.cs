6e913da854197ca6246cf4fcef9bc66acf23ab64
xxfast <isuru@xfastgames.com>
2017-09-22 20:43:05 +1000

    Updated score manager to grant score without distances, added the pickup behaviour and coin pickup behaviour, added coin images and animations and the sounds

---
+++ Assets/Scripts/CoinPickupBehavior.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class CoinPickupBehavior : PickupBehaviour {
+
+	public int pointsGranted;
+
+	public override void onPickup ()
+	{
+		GameManager.instance.GetComponent<ScoreManager> ().Score += pointsGranted;
+		GetComponent<Animator> ().SetTrigger ("Disappear");
+	}
+
+}
