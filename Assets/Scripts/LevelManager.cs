38739e92e07d2d7678853c7e97327700d4954ebd
xxfast <isuru@xfastgames.com>
2017-09-23 22:13:43 +1000

    Added the level manager and level profiles

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LevelManager : MonoBehaviour {
+
+	public List<LevelProfile> levels;
+
+	public int currentLevel;
+
+	[SerializeField] public UnityEvent onEnter; 
+
+	public int Level{
+		get {  return currentLevel; }
+		set { currentLevel = value; } 
+	}
+
+	public LevelProfile this [int i]{
+		get { return levels [i]; } 
+		set { levels [i] = value; } 
+	}
+
+	void Awake(){
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Add(LevelProfile toAdd){
+		levels.Add (toAdd);
+	}
+}
---
+++ Assets/Scripts/LevelProfile.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEditor;
+
+[System.Serializable]
+[CreateAssetMenu]
+public class LevelProfile: ScriptableObject {
+
+	[Header("Level Spawns")]
+	[SerializeField] public List<GameObject> spawns; 
+	[SerializeField] float spawnFrequency;
+
+	[Space(10)]
+
+	[Header("Level Options")]
+	[SerializeField] public float distance; 
+	[SerializeField] public Sprite background; 
+
+	[Space(5)]
+
+	[Header("Level Events")]
+	[SerializeField] public UnityEvent onEnter; 
+	[SerializeField] public UnityEvent onRandom; 
+	[SerializeField] public UnityEvent onExit; 
+
+	public LevelProfile ()
+	{
+	}
+
+
+}
+
