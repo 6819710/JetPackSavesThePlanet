777dcfe95fa621b063b718ad7789e47a6f282e49
Gavin Rossiter <rossiter.gavin@gmail.com>
2017-10-02 19:26:20 +1100

    The change that I missed in the commit

--- Assets/Scripts/WormHead.cs
+++ Assets/Scripts/WormHead.cs
@@ -8,10 +8,10 @@ public class WormHead : WormSegment {
     public int startSize = 1;
     public Transform mainTarget;
     public bool isDebug;
-    public float speed;
-    public float catchUpExponent =1f;
+    public float minSpeed;
+    public float catchUpExponent = 1f;
+    public float minDistanceBeforeCatchUp = 3f;
     public float maxSpeed = 100;
-
     public float attackAmount = 1;
     public float retreatTime = 1;
     private AIBehaviourStateController AIbsc;
@@ -19,6 +19,7 @@ public class WormHead : WormSegment {
     private Vector2 retreatTo;
 
     void Start() {
+        if (!mainTarget) mainTarget = GameObject.FindGameObjectWithTag("Player").transform;
         AIbsc = gameObject.GetComponent<AIBehaviourStateController>();
         AIbsc.ChangeState(AIBehaviourState.following);
         if (startSize < 1) startSize = 1;
@@ -27,22 +28,25 @@ public class WormHead : WormSegment {
 
     // Update is called once per frame
     void Update() {
-        
+
     }
 
     void FixedUpdate() {
         Debug.Log(AIbsc.GetCurrentState());
-        switch (AIbsc.GetCurrentState()){
-            case AIBehaviourState.debug: {
+        switch (AIbsc.GetCurrentState()) {
+            case AIBehaviourState.debug:
+                {
                     MoveWormHead(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane)), catchUpExponent);
                 }
                 break;
             case AIBehaviourState.idle: break;
-            case AIBehaviourState.following: {
+            case AIBehaviourState.following:
+                {
                     MoveWormHead(mainTarget.position, catchUpExponent);
                 }
                 break;
-            case AIBehaviourState.retreating: {
+            case AIBehaviourState.retreating:
+                {
                     MoveWormHead(retreatTo);
                 }
                 break;
@@ -53,23 +57,24 @@ public class WormHead : WormSegment {
 
     private void MoveWormHead(Vector2 targetPos, float catchUpExponent = 0) {
         Vector2 direction = targetPos - (Vector2)transform.position;
-        var modifiedSpeed = Mathf.Min(Mathf.Max(1, speed * Mathf.Pow(direction.magnitude, catchUpExponent), maxSpeed));
+        float catchUpFactor = Mathf.Max(1, Vector3.Distance(targetPos, transform.position) - minDistanceBeforeCatchUp);
+        var modifiedSpeed = Mathf.Min(minSpeed * Mathf.Max(1, Mathf.Pow(catchUpFactor, catchUpExponent)), maxSpeed);
         direction = direction.normalized * modifiedSpeed;
-        GetComponent<Rigidbody2D>().MovePosition((Vector2)transform.position + ( direction * Time.deltaTime));
+        GetComponent<Rigidbody2D>().MovePosition((Vector2)transform.position + (direction * Time.deltaTime));
     }
 
     private void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag == "Player") { //TODO Gavin: remove hardcoded tag
-			Health health = collision.gameObject.GetComponent<Health>();
-			if(health !=null){
-				health.dealDamage (attackAmount);
-			}
+            Health health = collision.gameObject.GetComponent<Health>();
+            if (health != null) {
+                health.dealDamage(attackAmount);
+            }
         }
-		if (collision.gameObject.layer == LayerMask.NameToLayer("Astroid")) { //TODO Isuru: remove hardcoded tag
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Astroid")) { //TODO Isuru: remove hardcoded tag
             //If wworm on screen then retreat when hittinng asteroids
             var screenCenterWorldCoordinates = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, Camera.main.transform.position.z));
             var screenBottomCenterWorldCoordinates = Camera.main.ViewportToWorldPoint(new Vector3(0.5f, 0f, Camera.main.transform.position.z));
-            if ( Vector3.Distance(mainTarget.position, transform.position) < Vector3.Distance(screenCenterWorldCoordinates, screenBottomCenterWorldCoordinates)) {
+            if (Vector3.Distance(mainTarget.position, transform.position) < Vector3.Distance(screenCenterWorldCoordinates, screenBottomCenterWorldCoordinates)) {
                 AIbsc.ChangeState(AIBehaviourState.retreating, retreatTime);
                 var normal = collision.contacts[0].normal;
                 var distance = collision.contacts[0].relativeVelocity.magnitude;
