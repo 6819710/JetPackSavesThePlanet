2176d34c3f3273eb97ff7bd09fce5c8db820f62d
xxfast <isuru@xfastgames.com>
2017-10-10 18:41:02 +1100

    Added the POW upgrade icon and implementation

---
+++ Assets/Scripts/Upgrades/POWUpgrade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+[CreateAssetMenu]
+public class POWUpgrade : ConsumableUpgrade {
+
+	[SerializeField] private float radius = 5;
+
+	public override void Activate ()
+	{
+		base.Activate ();
+	}
+
+	public POWUpgrade (string name): base(name)
+	{
+	}
+
+	public override void Effect ()
+	{
+		Debug.Log ("POW ACTIVATED!");
+		Restore ();
+	}
+}
