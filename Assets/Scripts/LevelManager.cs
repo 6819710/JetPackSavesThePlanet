8cb349e480079cd142d3e05f9fd738ad0ddc6248
xxfast <isuru@xfastgames.com>
2017-10-12 00:00:35 +1100

    Implemented time based termination conditions for the levels

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -41,6 +41,9 @@ public class LevelManager : MonoBehaviour {
 	void Update () {
 		if (currentLP != null) {
 			time += Time.deltaTime;
+			if(currentLP.condition is ITimable){
+				(currentLP.condition as ITimable).Process (Time.deltaTime);
+			}
 			if (time > Random.value * randomTickSpeed) {
 				time = 0;
 				RandomEvent ();
@@ -58,6 +61,9 @@ public class LevelManager : MonoBehaviour {
 	void StartLevel(int level){
 		if(level < levels.Count){
 			currentLP = this [level];
+			if(currentLP.condition is ITimable){
+				(currentLP.condition as ITimable).Start ();
+			}
 			BeginEvent ();
 		}
 	}
