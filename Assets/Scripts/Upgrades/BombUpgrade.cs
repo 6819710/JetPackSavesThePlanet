ef15f54ddd3b9377040d4570dc7a83444c67784f
xxfast <isuru@xfastgames.com>
2017-10-23 11:56:38 +1100

    updated fuse time and spawn ranges

--- Assets/Scripts/Upgrades/BombUpgrade.cs
+++ Assets/Scripts/Upgrades/BombUpgrade.cs
@@ -9,6 +9,8 @@ public class BombUpgrade : ConsumableUpgrade {
 	
 	public GameObject bombPrefab;
 	public float damageRadius = 1;
+	private float spawnMinRadius = 2;
+	private float spawnMaxRadius = 3;
 
 	public BombUpgrade (string name): base(name)
 	{
@@ -16,7 +18,15 @@ public class BombUpgrade : ConsumableUpgrade {
 
 	public override void Effect ()
 	{
-		Instantiate (bombPrefab, Owner.transform.position, new Quaternion());
+		Spawn ();
 		Restore ();
+	}
+
+	private void Spawn(){
+		float angle = Random.Range(0f, 360f);
+		float dist  = Random.Range(spawnMinRadius,spawnMaxRadius);
+		GameObject bomb = Instantiate (bombPrefab, Owner.transform.position, new Quaternion());
+		bomb.transform.Rotate(new Vector3(0, angle, 0));
+		bomb.transform.Translate(new Vector3(dist, 0, dist));
 	}
 }
