001dc80d2b73e47e61b797ae3c245a2aa5607187
James P. Galovic <jpgalovic@gmail.com>
2017-10-27 20:42:08 +1100

    Refine: Music Controller Script
    Cleaned Implementation of Music Controler, Relocated Prefab into  Controller Folder within Audio Prefabs Folder.
    
    Added: Music Tester Scene to seperate Music Testing from SFX Testing

--- Assets/Scripts/Audio/MusicController.cs
+++ Assets/Scripts/Audio/MusicController.cs
@@ -1,95 +1,131 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+/// <summary>
+/// Global Music Controller
+/// </summary>
 public class MusicController : MonoBehaviour {
 
-    public AudioMixerSnapshot calm;
-    public AudioMixerSnapshot calmBeat;
-    public AudioMixerSnapshot stress;
-    public AudioMixerSnapshot stressBeat;
-    public AudioMixerSnapshot silence;
-    public AudioMixerSnapshot panic;
-    public AudioMixerSnapshot panicBeat;
-    public AudioMixerSnapshot menu;
+    // Audio Mixer Snapshots
+    public AudioMixerSnapshot _calm;
+    public AudioMixerSnapshot _calmBeat;
 
-    public List<AudioSource> sorces;
+    public AudioMixerSnapshot _stress;
+    public AudioMixerSnapshot _stressBeat;
+    
+    public AudioMixerSnapshot _panic;
+    public AudioMixerSnapshot _panicBeat;
 
-    public float bpmNormal = 60.0f;
-    public float bpmIncreased = 75.0f;
-    public float bpmFast = 90.0f;
+    public AudioMixerSnapshot _silence;
+    public AudioMixerSnapshot _menu;
 
-    public float bpmBase = 60.0f;
+    public List<AudioSource> _sourceList;
 
-    public float bpm = 60.0f;
+    // BPM Alterations
+    public float _bpmNormal = 60.0f;
+    public float _bpmIncreased = 75.0f;
+    public float _bpmFast = 90.0f;
 
-    private float transition;
-    private float quaterNote;
+    private float _bpm = 60.0f;
 
-	// Use this for initialization
+    // Transistion Settings
+    private float _transition;
+    private float _quaterNote;
+
+    /// <summary>
+    /// Set Quaternote and Base Transisiton time on Start.
+    /// </summary>
 	void Start ()
     {
-        quaterNote = 60.0f / bpm;
-        transition = quaterNote;
+        _quaterNote = 60.0f / _bpm;
+        _transition = 0.0f;
         toMenu();
 	}
 
+    /// <summary>
+    /// Sets BPM on all Audio Sorces (Used to keep beats in sync).
+    /// </summary>
+    /// <param name="aBPM">BPM to change to</param>
     private void setBPM(float aBPM)
     {
-        float pitch = aBPM / bpm; //calculate putch setting from base bpm
+        float pitch = aBPM / _bpm; //calculate putch setting from base bpm
 
-        foreach(AudioSource s in sorces)
+        foreach(AudioSource s in _sourceList)
         {
             s.pitch = pitch;
         }
     }
 
+    /// <summary>
+    /// Transition Music to Calm Snapshot
+    /// </summary>
     public void toCalm()
     {
-        calm.TransitionTo(transition);
-        setBPM(bpmNormal);
+        _calm.TransitionTo(_transition);
+        setBPM(_bpmNormal);
     }
 
+    /// <summary>
+    /// Transition Music to Calm Beat Snapshot
+    /// </summary>
     public void toCalmBeat()
     {
-        calmBeat.TransitionTo(transition);
-        setBPM(bpmNormal);
+        _calmBeat.TransitionTo(_transition);
+        setBPM(_bpmNormal);
     }
 
+    /// <summary>
+    /// Transition Music to Stress Snapshot
+    /// </summary>
     public void toStress()
     {
-        stress.TransitionTo(transition);
-        setBPM(bpmIncreased);
+        _stress.TransitionTo(_transition);
+        setBPM(_bpmIncreased);
     }
 
+    /// <summary>
+    /// Transition Music to Stress Beat Snapshot
+    /// </summary>
     public void toStressBeat()
     {
-        stressBeat.TransitionTo(transition);
-        setBPM(bpmIncreased);
+        _stressBeat.TransitionTo(_transition);
+        setBPM(_bpmIncreased);
     }
 
-    public void toSilence()
-    {
-        silence.TransitionTo(transition);
-        setBPM(bpmNormal);
-    }
-
+    /// <summary>
+    /// Transition Music to Panic Snapshot
+    /// </summary>
     public void toPanic()
     {
-        panic.TransitionTo(transition);
-        setBPM(bpmFast);
+        _panic.TransitionTo(_transition);
+        setBPM(_bpmFast);
     }
 
+    /// <summary>
+    /// Transition Music to Panic Beat Snapshot
+    /// </summary>
     public void toPanicBeat()
     {
-        panicBeat.TransitionTo(transition);
-        setBPM(bpmFast);
+        _panicBeat.TransitionTo(_transition);
+        setBPM(_bpmFast);
     }
 
+    /// <summary>
+    /// Transition Music to Silence Snapshot
+    /// </summary>
+    public void toSilence()
+    {
+        _silence.TransitionTo(_transition);
+        setBPM(_bpmNormal);
+    }
+
+    /// <summary>
+    /// Transition Music to Menu Snapshot
+    /// </summary>
     public void toMenu()
     {
-        menu.TransitionTo(transition);
-        setBPM(bpmNormal);
+        _menu.TransitionTo(_transition);
+        setBPM(_bpmNormal);
     }
 }
